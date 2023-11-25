509497750407ba89dd66989ae4f895dc7ebd0d95
Hyungyu Lee <55528322+HyungyuLee-ant@users.noreply.github.com>
2023-11-25 12:05:12 +0900

    [Silver I] Title: IOIOI, Time: 352 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,15 +1,28 @@
 n = int(input())
 s = int(input())
 m = input()
-q = 'I'
+# q = 'I'
+i = 0
 count = 0
-for i in range(n):
-    q += 'OI'
+answer = 0
+# for i in range(n):
+#     q += 'OI'
 
-
-for i in range(n, s-n):
-    # print(m[i-n:i+n+1])
-    if m[i-n:i+n+1] == q:
+#단순
+# for i in range(n, s-n):
+#     # print(m[i-n:i+n+1])
+#     if m[i-n:i+n+1] == q:
+#         count += 1
+        
+while i < (s - 1):
+    if m[i:i+3] == 'IOI':
+        i += 2
         count += 1
+        if count == n:
+            answer += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
 
-print(count)
+print(answer)
