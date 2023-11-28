09baa17d831d6fe54a966b40ae42c6adeb0696d6
Hyungyu Lee <55528322+HyungyuLee-ant@users.noreply.github.com>
2023-11-28 22:17:48 +0900

    [Silver II] Title: 나무 자르기, Time: 4760 ms, Memory: 144340 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,23 @@
+trees, target = map(int, input().split())
+
+forest = list(map(int, input().split()))
+forest.sort(reverse=True)
+forest = tuple(forest)
+# print(forest)
+
+#bst를 해 봅시다
+height_low = 1
+height_high = max(forest)
+
+while height_low <= height_high:
+    height_mid = (height_high+height_low)//2
+    temp = 0
+    for tree in forest:
+        if tree > height_mid:
+            temp += tree-height_mid
+    if temp >= target:
+        height_low = height_mid + 1
+    elif temp < target:
+        height_high = height_mid - 1
+
+print(height_high)
