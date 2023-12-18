df24444db4837ce54ed78c70a5f5df9e94fe31ec
Hyungyu Lee <55528322+HyungyuLee-ant@users.noreply.github.com>
2023-12-18 17:00:55 +0900

    [Gold V] Title: 이진 검색 트리, Time: 2944 ms, Memory: 424776 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+#recursion error 방지
+sys.setrecursionlimit(10**9)
+
+arr = []
+while True:
+    try:
+        x = int(input())
+        arr.append(x)
+    except:
+        break
+    
+def solution(A):
+    if len(A)==0: 
+        return
+    else:
+        tempL, tempR = [],[]
+        mid = A[0]
+        for i in range(1, len(A)):
+            if A[i] > mid:
+                tempL = A[1:i]
+                tempR = A[i:]
+                break
+        else:
+            tempL = A[1:]
+        solution(tempL)
+        solution(tempR)
+        print(mid)
+
+solution(arr)
