d893da423c36ce7ca9f34b9657294c52e3582e99
Hyungyu Lee <55528322+HyungyuLee-ant@users.noreply.github.com>
2024-01-03 11:08:06 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+wavelength = int(input())
+if wavelength >= 620:
+    print('Red')
+elif wavelength >= 590:
+    print('Orange')
+elif wavelength >= 570:
+    print('Yellow')
+elif wavelength >= 495:
+    print('Green')
+elif wavelength >= 450:
+    print('Blue')
+elif wavelength >= 425:
+    print('Indigo')
+elif wavelength >= 380:
+    print('Violet')
