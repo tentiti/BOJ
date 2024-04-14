903139599e7dc90b5f54777541132b2ab4c7944b
Hyungyu Lee <55528322+tentiti@users.noreply.github.com>
2024-04-14 12:10:19 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.py
@@ -0,0 +1,8 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+if a==b==c==d:print('Fish At Constant Depth')
+elif a<b<c<d:print('Fish Rising')
+elif a>b>c>d:print('Fish Diving')
+else:print('No Fish')
