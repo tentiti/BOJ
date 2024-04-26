e149b56d26ba1a5689968a186c99708a32070f7d
Hyungyu Lee <55528322+tentiti@users.noreply.github.com>
2024-04-27 07:44:22 +0900

    [Bronze IV] Title: Quadrants, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.py
@@ -0,0 +1,14 @@
+while True:
+    a, b = map(float, input().split()) 
+    if a > 0 and b > 0:
+        print("Q1") 
+    elif a < 0 and b > 0:
+        print("Q2") 
+    elif a < 0 and b < 0:
+        print("Q3") 
+    elif a > 0 and b < 0:
+        print("Q4") 
+    else:
+        print("AXIS") 
+    if a == 0 and b == 0:
+        break  
