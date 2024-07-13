ea55da82fa78508ebe24b71d309dd798f98f3b9b
Hyungyu Lee <55528322+tentiti@users.noreply.github.com>
2024-07-13 12:53:42 +0900

    [Bronze I] Title: 세준세비, Time: 172 ms, Memory: 41452 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.py
+++ 백준/Bronze/1524. 세준세비/세준세비.py
@@ -1,28 +1,9 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-
-def winner(s, b, s_start, b_start):
-    if b[b_start] <= s[s_start]:
-        b_start += 1
-    else:
-        s_start += 1
-
-    if (s_start == n) and (b_start == m):
-        print('C')
-    elif b_start == m:
-        print('S')
-    elif s_start == n:
-        print('B')
-    else:
-        # print(s, b, s_start, b_start)
-        winner(s, b, s_start, b_start)
-
-
 for i in range(int(input())):
     t = input()
     n, m = map(int, input().split())
     s = sorted(list(map(int, input().split())))
     b = sorted(list(map(int, input().split())))
-    # print(s, b)
-    winner(s, b, 0, 0)
+    if s[-1] >= b[-1]:
+        print('S')
+    else:
+        print('B')
