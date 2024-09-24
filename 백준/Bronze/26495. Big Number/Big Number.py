feb4bc1614a7b6a6d05bb9e1c6b2e806495a083b
Hyungyu Lee <55528322+tentiti@users.noreply.github.com>
2024-09-24 19:46:26 +0900

    [Bronze IV] Title: Big Number, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26495. Big Number/Big Number.py
@@ -0,0 +1,62 @@
+for i in input():
+  if i =='0':
+    print('0000')
+    print('0  0')
+    print('0  0')
+    print('0  0')
+    print('0000')
+  elif i =='1':
+    print('   1')
+    print('   1')
+    print('   1')
+    print('   1')
+    print('   1')
+  elif i =='2':
+    print('2222')
+    print('   2')
+    print('2222')
+    print('2')
+    print('2222')
+  elif i =='3':
+    print('3333')
+    print('   3')
+    print('3333')
+    print('   3')
+    print('3333')
+  elif i =='4':
+    print('4  4')
+    print('4  4')
+    print('4444')
+    print('   4')
+    print('   4')
+  elif i =='5':
+    print('5555')
+    print('5')
+    print('5555')
+    print('   5')
+    print('5555')
+  elif i =='6':
+    print('6666')
+    print('6')
+    print('6666')
+    print('6  6')
+    print('6666')
+  elif i =='7':
+    print('7777')
+    print('   7')
+    print('   7')
+    print('   7')
+    print('   7')
+  elif i =='8':
+    print('8888')
+    print('8  8')
+    print('8888')
+    print('8  8')
+    print('8888')
+  elif i =='9':
+    print('9999')
+    print('9  9')
+    print('9999')
+    print('   9')
+    print('   9')
+  print()
